53d6b4e57baa8bf97d2a617fdacbe55b6317183d
Andrey Izman <izman.dev@gmail.com>
2015-07-03 13:47:17 +0300

    Remove extra Exception classes and interfaces

--- Exception/InvalidArgumentException.php
+++ Exception/InvalidArgumentException.php
@@ -8,7 +8,6 @@
 
 namespace SocialAuther\Exception;
 
-class InvalidArgumentException
-    extends \InvalidArgumentException
-    implements ExceptionInterface
-{}
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}
